65da8594d83c34926bb07676a3188220eef64cca
Janna <kittmaporx18@gmail.com>
2026-02-01 19:42:32 +0700

    รวมไฟล์งานของ Janna Hanna Hongyok และปรับปรุงเมนูหลัก

---
+++ lib/hanna_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class HongyokPage extends StatelessWidget {
+  const HongyokPage({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('หน้าของ Janna')),
+      body: Center(child: Text('ยินดีต้อนรับสู่ส่วนงานของ Janna')),
+    );
+  }
+}
